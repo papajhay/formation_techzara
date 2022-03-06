1203d4f79a0cfef2d0009e02cddcf7072810b43a
papajhay <onjanir.mpianina@gmail.com>
2022-03-06 13:16:11 +0100

    Second commit

--- test.php
+++ test.php
@@ -1,30 +1,116 @@
 <?php
-   class Volswagen
+   class Volswagen implements Motor
     {
         public $ires="firelli";
         protected $wheel="left";
-        private $door="four";
+        private $color="white";
 
-
+    /**
+     * Get the value of ires
+     */
         public function getIres() 
         {
+
             return $this->ires;
         }
 
-        // public function getWheel() 
-        // {
-        //     return $this->wheel;
-        // }
 
-        public function getDoor() 
+
+       /**
+        * Set the value of ires
+       */   
+         public function setIres($ires)
         {
-            return $this->door;
+            $this->ires = $ires;
+            return $this;
+        }
+
+
+        /**
+         * Get the value of wheel
+         */
+        public function getWheel() 
+        {
+
+            return $this->wheel;
+        }
+
+
+        /**
+         * Set the value of wheel
+         */
+
+        public function setWheel($wheel) 
+        {
+            $this->wheel = $wheel;
+            return $this;
+        }
+
+        public function getColor() 
+        {
+            
+            return $this->color;
+        }
+
+        public function setColor($color) 
+        {
+            $this->color = $color;
+            return $this;
+        }
+
+        public function getEnergy()
+        {
+           return "Essence";
         }
     }
-    $volswagen = new Volswagen();
-    echo $volswagen->ires;
-    // echo $volswagen->wheel;
-    echo $volswagen->door;
 
+
+    class Turbo extends Volswagen
+    {
+         /**
+     * Get the value of ires
+     */
+        public function getIres() 
+        {
+
+            return $this->ires;
+        }
+
+
+
+       /**
+        * Set the value of ires
+       */   
+         public function setIres($ires)
+        {
+            $this->ires = $ires;
+            return $this;
+        }
+    }
+
+
+    interface Motor 
+    {
+        public function getEnergy();
+    }
+
+
+    $volswagen = new Volswagen();
+    // Get the value of  the ires
+    echo $volswagen->ires  ;
+
+    // Set the value of  the ires
+    $volswagen->ires ="michelin";
+    echo $volswagen->ires  ;
+
+    echo $volswagen->getWheel();
+    $volswagen->setWheel("right");
+    echo $volswagen->getWheel();
+    
+    echo $volswagen->getColor();
+    $volswagen->setColor("red");
+    echo $volswagen->getColor();
+
+    echo $volswagen -> getEnergy();
 
 ?>
